86a6bde56ff1a6154f4f33519d3a37c178b04bb9
ukgroupe <ukgroupe@ukgroupes-MacBook.local>
2017-08-20 20:24:34 +0100

    first commit

---
+++ CoreProject/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CoreProject
+//
+//  Created by ukgroupe on 8/20/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var label: UILabel!
+    @IBOutlet var txt: UITextField!
+    @IBOutlet var login: UIButton!
+    @IBAction func loginAction(_ sender: Any) {
+    }
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var logout: UIButton!
+    @IBAction func logout2(_ sender: Any) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
